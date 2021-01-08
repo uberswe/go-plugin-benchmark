8c93ac1360fbc329739d727c9df49b62332bd6da
uberswe <markus@tenghamn.com>
2021-01-08 04:07:21 +0100

    updated gen.sh

--- plug/gen.sh
+++ plug/gen.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-protoc -I=. -I=$(go list -f '{{.Dir}}' github.com/elliotmr/plug/pkg/plugpb) --go_out=. --go_opt=module=uberswe/go-plugin-benchmark/plug --plug_out=. --plug_opt=module=uberswe/go-plugin-benchmark/plug plugin.proto
+protoc -I=. --go_out=. --go_opt=module=uberswe/go-plugin-benchmark/plug --plug_out=. --plug_opt=module=uberswe/go-plugin-benchmark/plug plugin.proto
